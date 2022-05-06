ea969174287c35d1eac54270a60bb55f3c037b75
franzruddy <salinasfranzruddy@gmail.com>
2022-05-05 22:17:44 -0400

    nueva modifi

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -5,7 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href=" {{asset('css/estilos.css')}}">
+    <link rel="stylesheet" href=" {{asset('lib/estilos.css')}}">
+    
+
+    <script src="https://kit.fontawesome.com/d41ce36db8.js" crossorigin="anonymous"></script>
+
+
+    <script src=" {{asset('lib/js/estilosapp')}} "></script>
+
 
     <title>Document</title>
 
@@ -13,7 +20,31 @@
 
 </head>
 <body>
-    <!--Heder-->
+    <!--Header-->
+    <header>
+        <nav>
+            <label class="logo1"><img src=" {{asset('img/logo2.png')}} " alt=""> </label>
+        </nav>
+        <nav class="menu">
+            <label class="logo">Web Cementerio</label>
+            <ul class="menu_items">
+                <li class="active " ><a  href="#"></a>Inicio</li>
+                <li ><a  href="#"></a>Contactos</li>
+                <li ><a  href="#"></a>Historia</li>
+                <li ><a  href="#"></a>Obituario</li>
+                <li ><a  href="#"></a>Servicios</li>
+            </ul>
+            <span class="btn_menu">
+                <i class="fa fa-bars"></i>
+            </span>
+        </nav>    
+    </header>
+
+@yield('content')
+
+<footer>
+
+</footer>
 
 </body>
 </html>
